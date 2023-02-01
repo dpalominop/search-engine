ca98667263c31e58586cefa20cdeeec45307a785
Daniel Palomino <dapalominop@gmail.com>
2023-02-01 00:02:05 -0600

    feat: add env variables in config file.

--- api/src/config.py
+++ api/src/config.py
@@ -14,3 +14,9 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 ROOT_PATH = os.getenv("ROOT_PATH", "/")
 
 CONCURRENT_REQUEST_PER_WORKER = int(os.getenv("CONCURRENT_REQUEST_PER_WORKER", "4"))
+
+BUCKET_NAME = os.getenv("BUCKET_NAME", "")
+S3_HOST = os.getenv("S3_HOST", "")
+
+BROKER_URL=os.getenv("BROKER_URL")
+REDIS_URL=os.getenv("REDIS_URL")
